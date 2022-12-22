79afd94f34bbd0b106c6bb9c1ad2e9694dd9ed3c
Mohammed fayis <90344973+mohmd-fayis@users.noreply.github.com>
2022-12-23 00:27:02 +0530

    initial commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
